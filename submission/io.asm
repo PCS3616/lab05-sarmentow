96448694cb973aeb7bb4408272ad1a92e4e2a5ea
Pedro Lins <pedrolins1309@gmail.com>
2025-02-22 13:46:02 -0300

    2nd attempt

--- submission/io.asm
+++ submission/io.asm
@@ -1,39 +1,32 @@
-@ /0008
-K /3030
-K /0010
-K /000a
-K /0100
-XPY K /0000
-SMT K /a000
-
-@ /10C
 GD /000 ; stores x
 MM /000 ; 
-GD /000 ; 
-MM /001 ;
 GD /000 ; space
 GD /000 ; store y
 MM /002 ; 
-GD /000 ;
-MM /003 ;
 LD /000 ; subtraindo 0x3030 de x
-SB /008 ; 
+SB A ; 
 MM /000 ;
 LD /002 ; subtraindo 0x3030 de y
-SB /008 ; 
+SB A ; 
 MM /002 ;
 AD /000 ; x + y -> AC
 MM XPY ; store x + y in 00C
-ML /00A
-ML /00A
-ML /00A
+ML B
+ML B
+ML B
 SB SMT
 JN FINISH ; if negative, that means the lsnibble is < a
 LD XPY
-SB /00C
-AD /00E
+SB C
+AD D
 MM XPY
 FINISH LD XPY
-       AD /008
+       AD A
        PD /100
        HM /000
+A K /3030
+B K /0010
+C K /000a
+D K /0100
+XPY K /0000
+SMT K /a000
